ed94ca7b138f73627451286c159d69b709a783fb
Nazareth <nazares2002@gmail.com>
2022-09-25 17:38:16 +0300

    section swiper

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,28 @@
-console.log("Hello, World!");
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+    OnlyInViewport: true,
+    pageUpDown: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
